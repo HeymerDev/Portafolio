c7cda7b57efcf05079a5f767b50c682c050c0426
HeymerDev <heymermeza11@gmail.com>
2026-06-04 23:04:11 -0500

    add work experience card and jobs & using this card on WorkExperience view

---
+++ src/data/jobs.ts
@@ -0,0 +1,10 @@
+import type { Jobs } from "@/types";
+
+export const jobs: Jobs[] = [
+  {
+    image: "../../src/assets/jobs/extituto.png",
+    jobNmae: "Extituto de Politica Abierta",
+    jobDescription: "Desarrollador Web FullStack",
+    workAges: "Mayo 2025 - Noviembre2 2025",
+  },
+];
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,10 @@ export interface Link {
   path: string;
   name: string;
 }
+
+export interface Jobs {
+  image: string;
+  jobNmae: string;
+  jobDescription: string;
+  workAges: string;
+}
