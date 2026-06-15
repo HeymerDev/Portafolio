d95dac92ed52eb5ba4026e465998c4325e992ac6
HeymerDev <heymermeza11@gmail.com>
2026-06-14 22:50:02 -0500

    add project data

---
+++ src/data/projects.ts
@@ -0,0 +1,16 @@
+import type { Project } from "@/types";
+
+export const projects: Project[] = [
+  {
+    name: "Project 1",
+    description: "Description of project 1",
+    links: [""],
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    name: "Project 1",
+    description: "Description of project 1",
+    links: [""],
+    image: "https://via.placeholder.com/150",
+  },
+];
