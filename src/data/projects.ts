80c6e82d08d54d29ca8d938f13be10ef6baaa1f3
HeymerDev <heymermeza11@gmail.com>
2026-06-14 23:17:23 -0500

    modified project data for real data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,15 +2,33 @@ import type { Project } from "@/types";
 
 export const projects: Project[] = [
   {
-    name: "Project 1",
-    description: "Description of project 1",
-    links: [""],
-    image: "https://via.placeholder.com/150",
+    name: "Dev-Tree",
+    description:
+      "Proyecto basado en linktree para desarrolladores, donde podras compartir tus redes sociales, y perfil de GitHub de una manera rapida y sencilla. <br/><br/>Tecnologias usadas: React, NodeJS, Express, MongoDB, TailwindCSS.",
+    link: "https://dev-tree-heymerdev.netlify.app/",
+    reposirories: [
+      "https://github.com/HeymerDev/dev-tree-frontend",
+      "https://github.com/HeymerDev/dev-tree-backend",
+    ],
+    image: "../assets/projects/devtree.webp",
   },
   {
-    name: "Project 1",
-    description: "Description of project 1",
-    links: [""],
-    image: "https://via.placeholder.com/150",
+    name: "CashTracker",
+    description:
+      "Proyecto de seguimiento de gastos personales, donde puedes registrar tus ingresos y gastos, y visualizar tus finanzas de una manera rapida y sencilla. <br/><br/>Tecnologias usadas: NextJS, NodeJS, Express, PostgreSQL, Sequelize, TailwindCSS.",
+    link: "",
+    reposirories: [
+      "https://github.com/HeymerDev/cash-tracker-backend",
+      "https://github.com/HeymerDev/cash-tracker-frontend",
+    ],
+    image: "../assets/projects/cashtracker.webp",
+  },
+  {
+    name: "Costeñol",
+    description:
+      "Mini compilador basado en un lenguaje de programación llamado Costeñol, el cual es un lenguaje de programación basado en español, con una sintaxis sencilla y facil de aprender. <br/><br/>Tecnologias usadas: JavaScript, NodeJS.",
+    link: "https://compilatorjs.netlify.app/",
+    reposirories: ["https://github.com/HeymerDev/compilator-js"],
+    image: "../assets/projects/compilator.webp",
   },
 ];
