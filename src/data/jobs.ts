80c12f682bb21ef9f165f050f4758b132383b21a
HeymerDev <heymermeza11@gmail.com>
2026-06-15 21:36:17 -0500

    moved jobs images and projects images on public

--- src/data/jobs.ts
+++ src/data/jobs.ts
@@ -2,7 +2,7 @@ import type { Jobs } from "@/types";
 
 export const jobs: Jobs[] = [
   {
-    image: "../../src/assets/jobs/extituto.png",
+    image: "/jobs/extituto.png",
     jobNmae: "Extituto de Politica Abierta",
     jobDescription: "Desarrollador Web FullStack",
     workAges: "Marzo 2025 - Octubre 2025",
